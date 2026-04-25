766f96e58d25eb94dd65079c5a7e19e8c86be6a6
carlos <therealcarlosediaz@gmail.com>
2026-04-24 18:53:09 -0700

    added functional forwarding unit

---
+++ FORWARDING_UNIT.sv
@@ -0,0 +1,25 @@
+module FORWARDING_UNIT( 
+      input logic [31:0] de_ex_rs1
+      input logic [31:0] de_ex_rs1
+      input logic [31:0] ex_mem_rs1
+      input logic [31:0] ex_mem_rs2
+      input logic [31:0] ex_mem_memwrite
+      input logic [31:0] ex_mem_regwrite
+      input logic [31:0] mem_wb_rs1
+      input logic [31:0] mem_wb_rs2
+      input logic [31:0] mem_wb_memwrite
+      input logic [31:0] mem_wb_regwrite
+      output logic fowardSEL
+
+  );
+   
+ always_comb begin
+
+
+ end
+
+
+
+
+
+end module
